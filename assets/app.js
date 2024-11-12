65ca9a7be9223baa96ebec8bec4615e82296bd0b
fran15simoes <144706478+fran15simoes@users.noreply.github.com>
2024-11-12 09:34:52 -0300

    ok

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,24 @@
 function criarCartao(categoria,pergunta,resposta){
-    //console.log(categoria,pergunta,resposta);
-    let container = document.getEelementbyId(´container`);
-    let cartao = document.creatElement(´article`);
-    cartao.className = ´cartao`;
-    
-    cartao.innerHTML =
-                `
-                <div class="cartao__conteudo">
-                    <h3programacao</h3>
-                    <div class="cartao__conteudo__resposta">
+ //console.log(categoria,pergunta,resposta);
+ let container = document.getEelementbyId('container');
+ //VARIAVEL CARTAO QUE <ARTICLE> NO HTML
+ let cartao = document.creatElement('article');
+ cartao.className = 'cartao';
+//CRIA CLASSE "cartao" para o elemento criado
+cartao.innerHTML = 
+ <div class="cartao__conteudo">
+ <h3>${categoria}</h3>
+ <div class="cartao__conteudo__pergunta">
+        <p>O que é JavaScript?</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>0
+             </p>
+    </div>
+    </div>
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('')
 }
